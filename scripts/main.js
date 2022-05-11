d5f49754db2967e83b72547181f61f37e71b29fa
aprilkrg <aprilkrgonzales@gmail.com>
2022-05-11 16:23:33 -0600

    console log the newly created input element

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,30 @@
 console.log("start of page")
 
+const getUserInput = function(){
+    console.log("get user input")
+    console.log(document.querySelector("#choice-input"), '<<< input el')
+}
+
+/* 
+GAME FUNCTION
+jobs:
+- log target of click event
+- invoke getUserInput function
+- save return from userInput to variable
+*/
 const gameFunction = function(e){
     console.log(e.target, "<< clicked")
-
+    let userChoice = getUserInput()
 }
+
+/*
+DOMContentLoaded EVENT LISTENER
+jobs:
+- console log message for dev
+- query DOM for startBtn 
+- save selected DOM element to variable
+- add event listener to that variable which invokes gameFunction
+*/
 document.addEventListener("DOMContentLoaded", () => {
     console.log("main.js loaded!")
     const startBtn = document.querySelector("#startBtn")
