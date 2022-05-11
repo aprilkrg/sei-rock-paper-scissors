b200d9e79e36b6f50076cc3b926dd7dc2ef2c6cc
aprilkrg <aprilkrgonzales@gmail.com>
2022-05-11 16:06:58 -0600

    create gameFunction and invoke on startButton click

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 console.log("start of page")
 
+const gameFunction = function(e){
+    console.log(e.target, "<< clicked")
+
+}
 document.addEventListener("DOMContentLoaded", () => {
     console.log("main.js loaded!")
     const startBtn = document.querySelector("#startBtn")
-    startBtn.addEventListener("click", (e) => {
-        console.log(e.target, "<< clicked")
-    })
+    startBtn.addEventListener("click", gameFunction)
 })
